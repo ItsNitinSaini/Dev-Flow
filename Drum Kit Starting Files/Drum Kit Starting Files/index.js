049ac1b271cd3f379880ad827abe9e32ad983894
ItsNitinSaini <142825241+ItsNitinSaini@users.noreply.github.com>
2026-05-03 13:04:24 +0530

    Added Animation

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -2,6 +2,7 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
@@ -9,6 +10,7 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 document.addEventListener("keydown", function (event) {
   var keyPressed = event.key;
   makeSound(keyPressed);
+  buttonAnimation(keyPressed);
 });
 
 function makeSound(key) {
@@ -46,3 +48,10 @@ function makeSound(key) {
   }
 }
 
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed")
+  },100)
+}
